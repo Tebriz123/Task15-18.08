96d19166acd9d586241b98967ab4d076c258574b
Tebriz123 <necefovtebriz5555@gmail.com>
2025-08-18 16:00:20 +0400

    initial commit

---
+++ Task015/Task015/Models/ListInt.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task015.Models
+{
+    internal class ListInt
+    {
+        private int[] arrey;
+
+        public int[] Arrey
+        {
+            get
+            {
+                return arrey;
+            }
+            set
+            {
+                arrey = value;
+            }
+        }
+        public int this[int index]
+        {
+            get
+            {
+                return arrey[index];
+            }
+            set
+            {
+                arrey[index] = value;
+            }
+        }
+
+        public void Add(int num)
+        {
+            Array.Resize(ref arrey, arrey.Length + 1);
+            arrey[arrey.Length-1] = num;    
+        }
+
+        public void AddRange(params int[] nums)
+        {
+            Array.Resize(ref arrey,arrey.Length + nums.Length);
+            for(int i = 0; i < nums.Length; i++)
+            {
+                arrey[arrey.Length-nums.Length+i] = nums[i];
+            }
+        }
+
+        public bool Contains(int num)
+        {
+            for(int i = 0;i < arrey.Length; i++)
+            {
+                if(this[i] == num)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public int Sum()
+        {
+            int sum = 0;
+            for( int i = 0;i < arrey.Length ; i++)
+            {
+                 sum+= this[i] ;
+            }
+            return sum;
+        }
+        public void Remove(int num)
+        {
+            int[] newArrey = new int[arrey.Length-1];
+            int j = 0;
+
+            for(int i = 0; i <  arrey.Length; i++)
+            {
+                if(arrey[i] != num)
+                {
+                    newArrey[j] = arrey[i];
+                    j++;
+                }
+            }
+            arrey = newArrey;
+
+        }
+        public void RemoveRange(params int[] nums)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                Remove(nums[i]);
+            }
+        }
+
+        public override string ToString()
+        {
+            return string.Join(", ", Arrey);
+        }
+
+    }
+}
---
+++ Task015/Task015/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using Task015.Models;
+
+namespace Task015
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           ListInt listInt = new ListInt()
+           {
+               Arrey = new int[] { 1,2,3,4,5,6,7}
+           };
+
+            foreach (int i in listInt.Arrey)
+            {
+                Console.WriteLine(i);
+            }
+
+            listInt.Remove(1);
+
+            Console.WriteLine("------------------");
+            foreach (int i in listInt.Arrey)
+            {
+                Console.WriteLine(i);
+            }
+
+            Console.WriteLine("--------------------------");
+
+
+            listInt.RemoveRange(2, 3, 5);
+
+            foreach (int i in listInt.Arrey)
+            {
+                Console.WriteLine(i);
+            }
+            listInt.ToString();
+            Console.WriteLine("------------------------");
+            listInt.Add(110);
+            foreach (int i in listInt.Arrey)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("---------------");
+
+            listInt.AddRange(12,14,18);
+            foreach(int i in listInt.Arrey)
+            {
+                Console.WriteLine(i);
+            }
+            Console.WriteLine("------------------");
+            Console.WriteLine(listInt.Contains(110));
+            Console.WriteLine(listInt.Contains(111));
+            Console.WriteLine(listInt.Sum());
+
+            Console.WriteLine(listInt.ToString());
+        }
+    }
+}
